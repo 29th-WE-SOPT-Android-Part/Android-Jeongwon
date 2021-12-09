797aa296576a8038db9e51bd1545d9b96f1d6d72
garden <garden@gardenui-MacBookPro.local>
2021-12-10 01:44:06 +0900

    [chore] layout naming edit

--- app/src/main/java/com/example/androidassignment/SignInActivity.kt
+++ app/src/main/java/com/example/androidassignment/SignInActivity.kt
@@ -1,9 +1,9 @@
 package com.example.androidassignment
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidassignment.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -15,32 +15,33 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
 
         initLoginBtn()
-
-        initJoinBtn()
+        initRegisterBtn()
 
         setContentView(binding.root)
 
     }
 
+
     private fun initLoginBtn() {
         binding.btnLogin.setOnClickListener {
             // 유저가 항목을 다 채우지 않았을 경우
             if(binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()){
-                Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
             }
             // 유저가 항목을 다 채웠을 경우
             else {
-                Toast.makeText(this, "환영합니다!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "환영합니다!", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
             }
         }
     }
 
-    private fun initJoinBtn() {
-        binding.btnJoin.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+    private fun initRegisterBtn() {
+        binding.btnRegister.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
     }
+
 }
--- app/src/main/java/com/example/androidassignment/SignUpActivity.kt
+++ app/src/main/java/com/example/androidassignment/SignUpActivity.kt
@@ -1,8 +1,8 @@
 package com.example.androidassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidassignment.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -13,23 +13,24 @@ class SignUpActivity : AppCompatActivity() {
 
         binding = ActivitySignUpBinding.inflate(layoutInflater)
 
-        initJoinFinishBtn()
+        initDoneRegisterBtn()
 
         setContentView(binding.root)
 
     }
 
-    fun initJoinFinishBtn() {
-        binding.btnJoinFinish.setOnClickListener {
+    fun initDoneRegisterBtn() {
+        binding.btnDoneRegister.setOnClickListener {
             // 유저가 항목을 다 채우지 않았을 경우
-            if(binding.etName.text.isNullOrBlank()|| binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()){
-                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_LONG).show()
+            if (binding.etName.text.isNullOrBlank() || binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
             }
             // 유저가 항목을 다 채웠을 경우
             else {
-                Toast.makeText(this, "회원가입 완료", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
                 finish()
             }
+
         }
     }
 }
